a0f0801fba35f518151e4c95437bb17489053a3b
rrufino71@gmail.com <rrufino71@gmail.com>
2023-04-20 13:40:17 -0300

    selecgt de coplores y talles

--- app/Http/Livewire/ProductoFront.php
+++ app/Http/Livewire/ProductoFront.php
@@ -3,17 +3,47 @@
 namespace App\Http\Livewire;
 
 use App\Models\Producto;
+use App\Models\Sku;
+use App\Models\Colores;
 use Livewire\Component;
 
 class ProductoFront extends Component
 {
     public $producto;
+    public $colores;
+    public $talles;
+    
     public $producto_id;
 
     public function mount($id)
     {
         $this->producto = Producto::where('id', $id)->firstOrFail();
-    }
+
+        $this->colores = Sku::select(['sku.color_id','colores.color'])
+        ->join('colores', 'sku.color_id', '=','colores.id')
+        ->where('sku.producto_id', '=', $id)
+        ->groupBy('sku.color_id','colores.color')
+        ->get();
+
+         dd($this->colores);
+
+
+        // $this->sku = Sku::select(['sku.id',
+        // 'sku.stock',
+        // 'sku.producto_id',
+        // 'productos.nombre',
+        // 'colores.color',
+        // 'talles.talle'])
+        //  ->join('productos', 'sku.producto_id', '=', 'productos.id')
+        //  ->join('talles', 'sku.talle_id', '=', 'talles.id')
+        //  ->join('colores', 'sku.color_id', '=', 'colores.id')
+        //  ->where('productos.nombre', 'like', '%' . $this->search . '%')
+        //  ->orderBy($this->sort, $this->order)
+        //  ->paginate(5);
+
+
+
+  }
 
     public function render()
     {
--- resources/views/livewire/producto-front.blade.php
+++ resources/views/livewire/producto-front.blade.php
@@ -1,35 +1,33 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+   
     <h1>Detalle del producto</h1>
 
 
-    {{-- <div class="grid grid-cols-12 gap-6 pt-8">
+ <div class="grid grid-cols-12 gap-6 pt-8">
         <div class="col-span-9">
             <div class="grid grid-cols-12 gap-6">
                 <div class="col-span-4">
-                    <img class="w-full h-96 object-cover object-center" src="{{ $producto->imagen }}"
-                        alt="{{ $producto->nombre }}">
+                    <img class="w-full h-96 object-cover object-center" src="{{$producto->imagen}}"
+                        alt="{{$producto->nombre}}">
                 </div>
                 <div class="col-span-8">
-                    <h2 class="text-gray-900 font-bold text-3xl mb-2">{{ $producto->nombre }}</h2>
-                    <p class="text-gray-700 text-base">{{ $producto->descripcion }}</p>
-                    <p class="text-gray-700 text-base font-bold mt-2">{{ $producto->precio }}</p>
-
+                    <h2 class="text-gray-900 font-bold text-3xl mb-2"> Nombre {{$producto->nombre}}</h2>
+                    <p class="text-gray-700 text-base">Desc. Corta {{$producto->descCorta}}</p>
+                    <p class="text-gray-700 text-base font-bold mt-2">Precio Lista {{$producto->precioLista}}</p>
+                    <p class="text-gray-700 text-base font-bold mt-2">Precio Oferta {{$producto->precioOferta}}</p>
+                    <p class="text-gray-700 text-base font-bold mt-2">Codigo {{$producto->codigo}}</p>
+                    
                     <div class="mt-4">
                         <label class="block font-bold text-gray-700">Talle:</label>
                         <select class="form-select mt-1 block w-full" id="talle">
-                            @foreach ($producto->talles as $talle)
-                                <option>{{ $talle->nombre }}</option>
-                            @endforeach
+                            
                         </select>
                     </div>
 
                     <div class="mt-4">
                         <label class="block font-bold text-gray-700">Color:</label>
                         <select class="form-select mt-1 block w-full" id="color">
-                            @foreach ($producto->colores as $color)
-                                <option>{{ $color->nombre }}</option>
-                            @endforeach
+                            
                         </select>
                     </div>
 
@@ -45,7 +43,7 @@
         <div class="col-span-3">
             {{-- @livewire('categorias-front') --}}
 </div>
-</div> --}}
+</div> 
 
 
 </div>
