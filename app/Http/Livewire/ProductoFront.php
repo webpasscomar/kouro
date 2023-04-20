3816a9797e7b55067a04f6bd2ae2dd14c30f42d0
rrufino71@gmail.com <rrufino71@gmail.com>
2023-04-20 16:29:07 -0300

    chequeo de stock

--- app/Http/Livewire/ProductoFront.php
+++ app/Http/Livewire/ProductoFront.php
@@ -5,19 +5,31 @@ namespace App\Http\Livewire;
 use App\Models\Producto;
 use App\Models\Sku;
 use App\Models\Colores;
+use App\Models\Talles;
 use Livewire\Component;
 
 class ProductoFront extends Component
 {
+   
+    //variables del formulario
+    public $talle_id,$color_id,$producto_id,$cantidad;
+
     public $producto;
     public $colores;
     public $talles;
+    public $disponibles;
     
-    public $producto_id;
 
     public function mount($id)
     {
+        $this->cantidad = 0;    
+        $this->disponibles = 0;
+        $this->talle_id = 0;
+        $this->color_id = 0;       
+        $this->producto_id = $id;         
         $this->producto = Producto::where('id', $id)->firstOrFail();
+
+
 
         $this->colores = Sku::select(['sku.color_id','colores.color'])
         ->join('colores', 'sku.color_id', '=','colores.id')
@@ -26,29 +38,50 @@ class ProductoFront extends Component
         ->get()
         ->toArray();
 
-      
-        // $this->sku = Sku::select(['sku.id',
-        // 'sku.stock',
-        // 'sku.producto_id',
-        // 'productos.nombre',
-        // 'colores.color',
-        // 'talles.talle'])
-        //  ->join('productos', 'sku.producto_id', '=', 'productos.id')
-        //  ->join('talles', 'sku.talle_id', '=', 'talles.id')
-        //  ->join('colores', 'sku.color_id', '=', 'colores.id')
-        //  ->where('productos.nombre', 'like', '%' . $this->search . '%')
-        //  ->orderBy($this->sort, $this->order)
-        //  ->paginate(5);
+
+        $this->talles = Sku::select(['sku.talle_id','talles.talle'])
+        ->join('talles', 'sku.talle_id', '=','talles.id')
+        ->where('sku.producto_id', '=', $id)
+        ->groupBy('sku.talle_id','talles.talle')
+        ->get()
+        ->toArray();
+    }
+
+    public function incrementa() 
+    {
+        $this->cantidad = $this->cantidad +1;
+        $this->checkstock();
+    }
 
 
+    public function decrementa() 
+    {
+        if ($this->cantidad > 1) {
+            $this->cantidad = $this->cantidad -1;
+            $this->checkstock();
+        }
+    }
 
-  }
+
+    public function checkstock() {
+        $dispo = 0;
+        if ($this->talle_id > 0 && $this->color_id > 0 && $this->cantidad > 0) {
+           $dispo = Sku::where('producto_id',$this->producto_id)
+           ->where('talle_id',$this->talle_id)
+           ->where('color_id',$this->color_id)
+           ->value('stock');
+           $this->disponibles = $dispo;
+
+       }
+       
+    }
 
     public function render()
     {
         // return view('livewire.producto-front')->layout('layouts.app');
         $producto = $this->producto;
-        $colores = $this->colores;
-        return view('livewire.producto-front', $producto,$colores);
+        $colores  = $this->colores;
+        $talles   = $this->talles;
+        return view('livewire.producto-front', $producto,$colores,$talles);
     }
 }
--- resources/views/livewire/producto-front.blade.php
+++ resources/views/livewire/producto-front.blade.php
@@ -11,6 +11,7 @@
                         alt="{{$producto->nombre}}">
                 </div>
                 <div class="col-span-8">
+                    <h2 class="text-gray-900 font-bold text-3xl mb-2" id="producto_id" wire:model="producto_id"> Id  {{$producto->id}}</h2>
                     <h2 class="text-gray-900 font-bold text-3xl mb-2"> Nombre {{$producto->nombre}}</h2>
                     <p class="text-gray-700 text-base">Desc. Corta {{$producto->descCorta}}</p>
                     <p class="text-gray-700 text-base font-bold mt-2">Precio Lista {{$producto->precioLista}}</p>
@@ -19,24 +20,46 @@
                     
                     <div class="mt-4">
                         <label class="block font-bold text-gray-700">Talle:</label>
-                        <select class="form-select mt-1 block w-full" id="talle">
-                        </select>
-                    </div>
-
-                    <div class="mt-4">
-                        <label class="block font-bold text-gray-700">Color:</label>
-                        <select class="form-select mt-1 block w-full" id="color">
-                            <option>Seleccione un Color</option>
-                            @foreach ($colores as $color)
-                                <option>{{ $color['color'] }}</option>
+                        <select class="form-select mt-1 block w-full"  id="talle_id" wire:model="talle_id" wire:change="checkstock()">
+                            <option value="0">Seleccione un Talle</option>
+                            @foreach ($talles as $talle)
+                                <option value="{{$talle['talle_id']}}">{{ $talle['talle'] }}</option>
                             @endforeach
                         </select>
                     </div>
 
                     <div class="mt-4">
+                        <label class="block font-bold text-gray-700">Color:</label>
+                        <select class="form-select mt-1 block w-full"  id="color_id" wire:model="color_id" wire:change="checkstock()">
+                            <option value="0">Seleccione un Color</option>
+                            @foreach ($colores as $color)
+                                <option value="{{$color['color_id']}}">{{ $color['color'] }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="mt-4">
+                        <button class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-md 
+                                       shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                                       wire:click.prevent="incrementa()" >
+                            +
+                        </button>
+                        <input class="py-2 px-2 " type="numeric"  id="cantidad" wire:model="cantidad" wire:change="checkstock()"></input> 
+                        <button class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-md 
+                                      shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" 
+                                      wire:click.prevent="decrementa()" >
+                            -
+                        </button>
+                        Disponibles<input class="py-2 px-2 " type="numeric"  id="disponibles" wire:model="disponibles" disabled></input> 
+                    </div>
+                 
+                    <div class="mt-4">
                         <button
-                            class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                            Agregar al carrito
+                            
+                            class="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-bold rounded-md 
+                            shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2
+                            {{ $cantidad > $disponibles ? 'disabled' : '' }}
+                            "> 
+                            Agregar al carrito  {{ $cantidad > $disponibles ? '(sin stock)' : '' }}
                         </button>
                     </div>
                 </div>
