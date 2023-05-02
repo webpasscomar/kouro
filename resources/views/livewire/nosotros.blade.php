aa8d05ceedc1496cb5f033df65547d5e5f511488
Leandro Wagner <leanw1711@gmail.com>
2023-05-02 11:33:45 -0300

    Pruebas de Lean

--- resources/views/livewire/nosotros.blade.php
+++ resources/views/livewire/nosotros.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px6 lg:px-8">
 
-            <div class="grid md:grid-cols-2">
+            <!-- <div class="grid md:grid-cols-1">
                 <div>
                     <div class="flex justify-center">
                         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
@@ -27,8 +27,8 @@
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div class="flex justify-center">
+                <>
+                    <!-- <div class="flex justify-center">
                         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
                             <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
                                 src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt="" />
@@ -42,10 +42,36 @@
                                 <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
                             </div>
                         </div>
+                    </div> -->
+                <!-- </div> -->
+            <!-- </div> -->
+
+            <!-- Información de cada sucursal -->
+        
+            <div class="grid grid-cols-3 gap-x-11 rounded-lg shadow-lg shadow-gray-500 hover:shadow-gray-700 bg-red-500">
+
+                <!-- Imagen y descripción de la sucursal -->
+                <div class="max-w-sm bg-red-700">
+                    <a href="#">
+                        <img class="w-full h-96 md:h-64 object-cover md:rounded-none"
+                            src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt="" />
+                    </a>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-slate-100">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </a>
                     </div>
                 </div>
+                <!-- Google Maps -->
+                <div class="w-full mx-auto my-auto">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3287.7824418513887!2d-58.52391016947474!3d-34.508399390813125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb0ee292497e9%3A0xf9a611e58747f528!2sUnicenter!5e0!3m2!1ses!2sar!4v1683031606595!5m2!1ses!2sar" width="750" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
-
 
 
 
