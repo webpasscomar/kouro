35a5c39aa1dfe6ea5be16dd26fe8e4a06f28e027
rrufino71@gmail.com <rrufino71@gmail.com>
2023-05-04 16:06:56 -0300

    pago con mercadolibre

--- app/Http/Controllers/MpController.php
+++ app/Http/Controllers/MpController.php
@@ -22,17 +22,23 @@ class MpController extends Controller
             );
 
 
-            $response_jason = json_decode($response);
+            $response_json = json_decode($response);
             //dump($response_jason->external_reference);
             //dump($response_jason->status);
             //dump($response_jason);
-            dump($response_jason->notification_url);
+            //dump($response_jason->notification_url);
 
 
-
-
-
-
+              //actualiza cabecera
+             Log_pago::Create(
+                [
+                    'idpedido'        => $response_json->external_reference,
+                    'operacion_pago'  => $payment_id,
+                    'status'          => $response_json->status,
+                    'log'             => $response,
+                ]);
+                //$this->emit('mensajePositivo', ['mensaje' => 'El pago se realizo con exito']);
+                //redirect()->to('/shop');
 
     }
 }
