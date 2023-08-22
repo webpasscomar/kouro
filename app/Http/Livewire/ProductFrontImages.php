79db5da41bbbf12a85930bff6e49841adf501b4e
Eduardo Passarelli <epassarelli@gmail.com>
2023-08-22 15:20:54 -0300

    Se corrige el producto con error en la vista

--- app/Http/Livewire/ProductFrontImages.php
+++ app/Http/Livewire/ProductFrontImages.php
@@ -12,16 +12,11 @@ class ProductFrontImages extends Component
     public function mount($images)
     {
         $this->images = $images;
-<<<<<<< HEAD
-        $this->selectedImages = $images[0];
-=======
         if ($images) {
             $this->selectedImages = $images[0];
-        }else{
+        } else {
             $this->selectedImages = null;
         }
-
->>>>>>> 288272709c91085d02ad8d5cd6e339fa9fd4bb08
     }
 
     public function selectImage($image)
@@ -31,12 +26,6 @@ class ProductFrontImages extends Component
 
     public function render()
     {
-<<<<<<< HEAD
-=======
-
-        // dd($this->images, $this->selectedImages);
-
->>>>>>> 288272709c91085d02ad8d5cd6e339fa9fd4bb08
         return view('livewire.product-front-images');
     }
 }
