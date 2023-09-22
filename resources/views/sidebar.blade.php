8c8e4a6a1ea185abf4324986a1bffd3d8c4b2ab2
Leandro Wagner <leanw1711@gmail.com>
2023-09-22 14:01:02 -0300

    Se modifico Sidebar Backend, categorias los items

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -6,27 +6,35 @@
     </div> --}}
 
 <!-- Sidebar a la derecha -->
-<aside class="w-auto bg-gray-800 text-white p-4 rounded-lg ml-4 pl-4">
+<aside class="w-auto bg-gray-800 text-white p-4 rounded-lg ml-4 pl-4 overflow-hidden mt-11">
     <!-- Logo o título del sidebar -->
-    <div class="text-xl font-semibold mb-6">
+    {{-- <div class="text-xl font-semibold mb-6">
         Mi panel
-    </div>
+    </div> --}}
+
+    {{-- Sidebar  --}}
+    <h4>ADMINISTRACION</h4>
 
     <!-- Enlaces del sidebar -->
-    <ul>
-        <li class="mb-2">
-            <a href="{{ route('colores') }}" class="text-gray-300 hover:text-white">
-                Colores
-            </a>
-        </li>
+    <ul class="list-disc px-7 mt-3">
         <li class="mb-2">
             <a href="{{ route('categorias') }}" class="text-gray-300 hover:text-white">
                 Categorias
             </a>
         </li>
         <li class="mb-2">
-            <a href="{{ route('productos') }}" class="text-gray-300 hover:text-white">
-                Productos
+            <a href="{{ route('colores') }}" class="text-gray-300 hover:text-white">
+                Colores
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="{{ route('formasdeentrega') }}" class="text-gray-300 hover:text-white">
+                Formas de entrega
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="{{ route('presentaciones') }}" class="text-gray-300 hover:text-white">
+                Presentaciones
             </a>
         </li>
         <li class="mb-2">
@@ -35,8 +43,48 @@
             </a>
         </li>
         <li class="mb-2">
-            <a href="{{ route('presentaciones') }}" class="text-gray-300 hover:text-white">
-                Presentaciones
+            <a href="{{ route('talles') }}" class="text-gray-300 hover:text-white">
+                Talles
+            </a>
+        </li>
+    </ul>
+
+    <h4 class="mt-5">VENTAS</h4>
+
+    <ul class="list-disc px-7 mt-3">
+        <li class="mb-2">
+            <a href="{{ route('productos') }}" class="text-gray-300 hover:text-white">
+                Productos
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="{{ route('pedidos') }}" class="text-gray-300 hover:text-white">
+                Pedidos
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="{{ route('movimientos') }}" class="text-gray-300 hover:text-white">
+                Movimientos (Stock)
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="{{ route('pendientes') }}" class="text-gray-300 hover:text-white">
+                Pendientes (Stock)
+            </a>
+        </li>
+        <li class="mb-2">
+            <a href="{{ route('historias') }}" class="text-gray-300 hover:text-white">
+                Historias (Movimientos)
+            </a>
+        </li>
+    </ul>
+
+    <h4 class="mt-5">WEB</h4>
+
+    <ul class="list-disc px-7 mt-3">
+        <li class="mb-2">
+            <a href="{{ route('galerias') }}" class="text-gray-300 hover:text-white">
+                Galeria
             </a>
         </li>
         <li class="mb-2">
@@ -47,41 +95,6 @@
         <li class="mb-2">
             <a href="{{ route('testimonios') }}" class="text-gray-300 hover:text-white">
                 Testimonios
-            </a>
-        </li>
-        <li class="mb-2">
-            <a href="{{ route('formasdeentrega') }}" class="text-gray-300 hover:text-white">
-                Formas de entrega
-            </a>
-        </li>
-        <li class="mb-2">
-            <a href="{{ route('talles') }}" class="text-gray-300 hover:text-white">
-                Talles
-            </a>
-        </li>
-        <li class="mb-2">
-            <a href="{{ route('movimientos') }}" class="text-gray-300 hover:text-white">
-                Movimientos
-            </a>
-        </li>
-        <li class="mb-2">
-            <a href="{{ route('historias') }}" class="text-gray-300 hover:text-white">
-                Historias
-            </a>
-        </li>
-        <li class="mb-2">
-            <a href="{{ route('pendientes') }}" class="text-gray-300 hover:text-white">
-                Pendientes
-            </a>
-        </li>
-        <li class="mb-2">
-            <a href="{{ route('galerias') }}" class="text-gray-300 hover:text-white">
-                Galerias
-            </a>
-        </li>
-        <li class="mb-2">
-            <a href="{{ route('pedidos') }}" class="text-gray-300 hover:text-white">
-                Pedidos
             </a>
         </li>
         <!-- Agrega más enlaces según tus necesidades -->
