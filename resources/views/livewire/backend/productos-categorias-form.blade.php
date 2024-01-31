382ac937198fafeb5bcd5a857165cb17bab31e2d
= <=>
2024-01-31 15:11:13 -0300

    se modificó la tabla productos imágenes agregando columna color_id

--- resources/views/livewire/backend/productos-categorias-form.blade.php
+++ resources/views/livewire/backend/productos-categorias-form.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed z-5 inset-0 overflow-y-auto ease-out duration-400">
+<div class="fixed z-50 inset-0 overflow-y-auto ease-out duration-400">
     <div class="flex justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
         <div class="fixed inset-0 transition-opacity">
@@ -7,16 +7,19 @@
 
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
 
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        <div
+            class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
             role="dialog" aria-modal="true" aria-labelledby="modal-headline">
 
             <h3 class="text-black font-bold flex justify-center">{{$producto_nombre}}</h3>
 
             <h2 class="p-2">Ingreso de Categoria</h2>
-            <form enctype="multipart/form-data">   {{--wire:submit.prevent="saveImagen" enctype="multipart/form-data" --}}
+            <form
+                enctype="multipart/form-data">   {{--wire:submit.prevent="saveImagen" enctype="multipart/form-data" --}}
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 grid grid-cols-1 sm:grid-cols-2 gap-2">
                     <div class="mb-3">
-                        <label for="presentacion_id" class="block text-gray-700 text-sm font-bold mb-2">Categoria:</label>
+                        <label for="presentacion_id"
+                               class="block text-gray-700 text-sm font-bold mb-2">Categoria:</label>
                         <select
                             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             wire:model="categoria_id">
@@ -25,19 +28,19 @@
                                 <option value="{{ $categoria->id }}">{{ $categoria->categoria }}</option>
                             @endforeach
                         </select>
-                        <x-jet-input-error for="categoria_id" />
+                        <x-jet-input-error for="categoria_id"/>
 
                     </div>
 
                     <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse col-span-2">
                         <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
                                 <button wire:click.prevent="saveCategoria" type="button"
-                                class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-purple-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-purple-800 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">Guardar</button>
+                                        class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-purple-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-purple-800 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">Guardar</button>
 
                         </span>
                         <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
                                 <button wire:click.prevent="notsaveCategoria" type="button"
-                                class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-gray-200 text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">Cancelar</button>
+                                        class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-gray-200 text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">Cancelar</button>
                         </span>
                     </div>
 
