be791a04de73804b6081c225c6e09e8aa30fe901
Eduardo Passarelli <epassarelli@gmail.com>
2024-02-05 12:45:10 -0300

    Se actualizan vistas nosotros y sucursales. Se suben imagenes de locales

--- resources/views/livewire/nosotros.blade.php
+++ resources/views/livewire/nosotros.blade.php
@@ -1,68 +1,68 @@
 <div>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Nosotros
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      Nosotros
+    </h2>
+  </x-slot>
 
-    <!-- Sección Kouro Linea y ¿Que Ofrecemos? -->
-    <div class="py-8">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <!-- Sección Kouro Linea  -->
-            <div class="grid md:grid-cols-2 gap-10 md:gap-y-32 gap-y-16">
-                <div class="md:bg-gray-600 rounded-lg md:max-w-lg md:m-auto">
-                    <img src="https://kouro.webpass.online/assets/uploads/7/local1.jpg" alt="Kouro Tienda"
-                        class="border-white border-8 rounded-lg shadow-xl w-full h-full md:-translate-x-10 md:translate-y-10">
-                </div>
-                <div class="m-auto">
-                    <h2 class="text-2xl md:text-3xl font-normal font-sans">¿Cómo surge <strong
-                            class="font-extrabold">Kouro Linea Fina</strong> y con qué propósito?</h2>
-                    <p class="text-lg text-justify md:text-xl mt-10 font-sans">A principio de los 90, Kouro fue comenzó
-                        con la confección de productos xxxx en forma artesanal que se comercialiban xxxx. En una segunda
-                        etapa, se creó o abrió el local</p>
-                </div>
-                <!-- Sección ¿Que Ofrecemos?  -->
-                <div>
-                    <h2 class="text-2xl md:text-3xl font-sans font-medium">¿Qué <strong
-                            class="font-extrabold">Ofrecemos?</strong></h2>
-                    <p class="text-lg mt-4 font-sans">Atención personalizada y especializada.</p>
-                    <div class="flex mt-10 items-center py-5">
-                        <div>
-                            <i class="fa fa-check text-red-600"></i>
-                        </div>
-                        <div class="ml-4 text-md">
-                            <p><b>Otros productos -</b> Tenemos Cinturones, Billeteras, Paraguas y otros hermosos
-                                accesorios para vos o para regalar.</p>
-                        </div>
-                    </div>
-                    <div class="flex mt-2 items-center py-5">
-                        <div>
-                            <i class="fa fa-check text-red-600"></i>
-                        </div>
-                        <div class="ml-4 text-md">
-                            <p><b>Ventas minorista y mayorista -</b> Contamos con envíos a domicilio por moto en
-                                cercanías de los locales y por Andreani en el resto del pais.</p>
-                        </div>
-                    </div>
-                    <div class="flex mt-2 items-center py-5">
-                        <div>
-                            <i class="fa fa-check text-red-600"></i>
-                        </div>
-                        <div class="ml-4 text-md">
-                            <p><b>Otros productos -</b> Tenemos Cinturones, Billeteras, Paraguas y otros hermosos
-                                accesorios para vos o para regalar.</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Imágenes superpuestas  -->
-                <div class="md:relative md:pl-10 md:m-auto md:mt-14">
-                    <img src="https://kouro.webpass.online/assets/uploads/7/localH.jpg" alt="productos"
-                        class="md:w-72 w-full mb-10 md:mb-0 border-white border-8 rounded-lg shadow-xl md:absolute md:-translate-x-10 md:-translate-y-14">
-                    <img src="https://kouro.webpass.online/assets/uploads/7/local2.jpg" alt="productos"
-                        class="w-full border-white border-8 rounded-lg shadow-xl">
-                </div>
-            </div>
+  <!-- Sección Kouro Linea y ¿Que Ofrecemos? -->
+  <div class="py-8">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <!-- Sección Kouro Linea  -->
+      <div class="grid md:grid-cols-2 gap-10 md:gap-y-32 gap-y-16">
+        <div class="md:bg-gray-600 rounded-lg md:max-w-lg md:m-auto">
+          <img src="{{ asset('img/local1.jpg') }}" alt="Kouro Tienda"
+            class="border-white border-8 rounded-lg shadow-xl w-full h-full md:-translate-x-10 md:translate-y-10">
         </div>
+        <div class="m-auto">
+          <h2 class="text-2xl md:text-3xl font-normal font-sans">¿Cómo surge <strong class="font-extrabold">Kouro Linea
+              Fina</strong> y con qué propósito?</h2>
+          <p class="text-lg text-justify md:text-xl mt-10 font-sans">A principio de los 90, Kouro fue comenzó
+            con la confección de productos xxxx en forma artesanal que se comercialiban xxxx. En una segunda
+            etapa, se creó o abrió el local</p>
+        </div>
+        <!-- Sección ¿Que Ofrecemos?  -->
+        <div>
+          <h2 class="text-2xl md:text-3xl font-sans font-medium">¿Qué <strong class="font-extrabold">Ofrecemos?</strong>
+          </h2>
+          <p class="text-lg mt-4 font-sans">Atención personalizada y especializada.</p>
+          <div class="flex mt-10 items-center py-5">
+            <div>
+              <i class="fa fa-check text-red-600"></i>
+            </div>
+            <div class="ml-4 text-md">
+              <p><b>Otros productos -</b> Tenemos Cinturones, Billeteras, Paraguas y otros hermosos
+                accesorios para vos o para regalar.</p>
+            </div>
+          </div>
+          <div class="flex mt-2 items-center py-5">
+            <div>
+              <i class="fa fa-check text-red-600"></i>
+            </div>
+            <div class="ml-4 text-md">
+              <p><b>Ventas minorista y mayorista -</b> Contamos con envíos a domicilio por moto en
+                cercanías de los locales y por Andreani en el resto del pais.</p>
+            </div>
+          </div>
+          <div class="flex mt-2 items-center py-5">
+            <div>
+              <i class="fa fa-check text-red-600"></i>
+            </div>
+            <div class="ml-4 text-md">
+              <p><b>Otros productos -</b> Tenemos Cinturones, Billeteras, Paraguas y otros hermosos
+                accesorios para vos o para regalar.</p>
+            </div>
+          </div>
+        </div>
+        <!-- Imágenes superpuestas  -->
+        <div class="md:relative md:pl-10 md:m-auto md:mt-14">
+          <img src="{{ asset('img/localH.jpg') }}" alt="productos"
+            class="md:w-72 w-full mb-10 md:mb-0 border-white border-8 rounded-lg shadow-xl md:absolute md:-translate-x-10 md:-translate-y-14">
+          <img src="{{ asset('img/loca21.jpg') }}" alt="productos"
+            class="w-full border-white border-8 rounded-lg shadow-xl">
+        </div>
+      </div>
     </div>
+  </div>
 </div>
--- resources/views/livewire/sucursales.blade.php
+++ resources/views/livewire/sucursales.blade.php
@@ -1,66 +1,64 @@
 <div>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Sucursales
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      Sucursales
+    </h2>
+  </x-slot>
 
-    <!-- Información de cada sucursal -->
+  <!-- Información de cada sucursal -->
 
-    <!-- Sucursal Morón -->
+  <!-- Sucursal Morón -->
 
-    <div class="py-8">
-        <div class="max-w-7xl mx-auto md:px-8">
-            <div
-                class="grid grid-cols-1 md:grid-cols-3 mt-5 rounded-lg md:gap-x-4 shadow-lg shadow-gray-500 bg-gray-300">
-                <!-- Imagen y descripción de la sucursal -->
-                <div class="bg-gray-300 p-6 rounded-lg">
-                    <img class="w-full h-96 md:h-64 object-cover rounded-lg"
-                        src="https://kouro.webpass.online/assets/uploads/7/local1.jpg" alt="sucursal Moron" />
-                    <div class="mt-5">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-700 dark:text-white">Sucursal Morón
-                        </h5>
-                        <h6 class="mb-3 font-normal">Almte. Brown 744</h6>
-                        <p>Lunes a Sábados de 9:00 a 19:30 hs</p>
-                        <p class=" font-semibold">Domingos Cerrado</p>
-                        <p class="mt-3"><strong>Tel 11-4629-4476</strong></p>
-                    </div>
-                </div>
-                <!-- Google Maps -->
-                <div class="aspect-auto p-6 md:col-span-2">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.081374353732!2d-58.62018834310188!3d-34.65059256748799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc766ccb08501%3A0x8be78c8a0de27125!2sKouro%20Linea%20Fina%20-%20Cuero%20Argentino!5e0!3m2!1ses!2sar!4v1674231149687!5m2!1ses!2sar"
-                        style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                        class="w-full md:h-full rounded-lg h-64"></iframe>
-                </div>
-            </div>
-
-            <!-- Sucursal Ramos Mejía -->
-
-            <div
-                class="grid grid-cols-1 md:grid-cols-3 mt-20 rounded-lg md:gap-x-4 shadow-lg shadow-gray-500 bg-gray-300">
-
-                <!-- Imagen y descripción de la sucursal -->
-                <div class="bg-gray-300 p-6 rounded-lg">
-                    <img class="w-full h-96 md:h-64 object-cover rounded-lg"
-                        src="https://kouro.webpass.online/assets/uploads/7/local1.jpg" alt="sucursal Moron" />
-                    <div class="mt-5">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-700 dark:text-white">Sucursal Ramos
-                            Mejía</h5>
-                        <h6 class="mb-3 font-normal ">Av. de Mayo 32</h6>
-                        <p>Lunes a Sábados de 10:00 a 14:00 hs y de 15:00 a 19:00 hs</p>
-                        <p class=" font-semibold">Domingos Cerrado</p>
-                        <p class="mt-3"><strong>Tel 11-4658-0343</strong></p>
-                    </div>
-                </div>
-                <!-- Google Maps -->
-                <div class="aspect-auto p-6 md:col-span-2">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.5337033793307!2d-58.567683284946625!3d-34.64122218044985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc7e56394cfc5%3A0xb5e898c27e5a80a6!2sKouro%20Linea%20Fina%20-%20Cuero%20Argentino!5e0!3m2!1ses!2sar!4v1674235251006!5m2!1ses!2sar"
-                        style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                        class="w-full md:h-full rounded-lg h-64"></iframe>
-                </div>
-            </div>
+  <div class="py-8">
+    <div class="max-w-7xl mx-auto md:px-8">
+      <div class="grid grid-cols-1 md:grid-cols-3 mt-5 rounded-lg md:gap-x-4 shadow-lg shadow-gray-500 bg-gray-300">
+        <!-- Imagen y descripción de la sucursal -->
+        <div class="bg-gray-300 p-6 rounded-lg">
+          <img class="w-full h-96 md:h-64 object-cover rounded-lg" src="{{ asset('img/local1.jpg') }}"
+            alt="sucursal Moron" />
+          <div class="mt-5">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-700 dark:text-white">Sucursal Morón
+            </h5>
+            <h6 class="mb-3 font-normal">Almte. Brown 744</h6>
+            <p>Lunes a Sábados de 9:00 a 19:30 hs</p>
+            <p class=" font-semibold">Domingos Cerrado</p>
+            <p class="mt-3"><strong>Tel 11-4629-4476</strong></p>
+          </div>
         </div>
+        <!-- Google Maps -->
+        <div class="aspect-auto p-6 md:col-span-2">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1641.081374353732!2d-58.62018834310188!3d-34.65059256748799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc766ccb08501%3A0x8be78c8a0de27125!2sKouro%20Linea%20Fina%20-%20Cuero%20Argentino!5e0!3m2!1ses!2sar!4v1674231149687!5m2!1ses!2sar"
+            style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+            class="w-full md:h-full rounded-lg h-64"></iframe>
+        </div>
+      </div>
+
+      <!-- Sucursal Ramos Mejía -->
+
+      <div class="grid grid-cols-1 md:grid-cols-3 mt-20 rounded-lg md:gap-x-4 shadow-lg shadow-gray-500 bg-gray-300">
+
+        <!-- Imagen y descripción de la sucursal -->
+        <div class="bg-gray-300 p-6 rounded-lg">
+          <img class="w-full h-96 md:h-64 object-cover rounded-lg" src="{{ asset('img/local2.jpg') }}"
+            alt="sucursal Moron" />
+          <div class="mt-5">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-700 dark:text-white">Sucursal Ramos
+              Mejía</h5>
+            <h6 class="mb-3 font-normal ">Av. de Mayo 32</h6>
+            <p>Lunes a Sábados de 10:00 a 14:00 hs y de 15:00 a 19:00 hs</p>
+            <p class=" font-semibold">Domingos Cerrado</p>
+            <p class="mt-3"><strong>Tel 11-4658-0343</strong></p>
+          </div>
+        </div>
+        <!-- Google Maps -->
+        <div class="aspect-auto p-6 md:col-span-2">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.5337033793307!2d-58.567683284946625!3d-34.64122218044985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc7e56394cfc5%3A0xb5e898c27e5a80a6!2sKouro%20Linea%20Fina%20-%20Cuero%20Argentino!5e0!3m2!1ses!2sar!4v1674235251006!5m2!1ses!2sar"
+            style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+            class="w-full md:h-full rounded-lg h-64"></iframe>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
