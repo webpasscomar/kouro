cdbe9ac595b1a3715f8a7a4c4e85ba6721f808e8
Eduardo Passarelli <epassarelli@gmail.com>
2024-10-04 10:13:10 -0300

    Se eliminan archivos en desuso

--- resources/views/productos/categoria.blade.php
+++ resources/views/productos/categoria.blade.php
@@ -10,53 +10,53 @@
     <div class="row">
       {{-- Sección principal --}}
       <div class="col-md-9">
+        <div class="row">
+          @if (count($hijas) > 0)
 
-        @if (count($hijas) > 0)
+            @foreach ($hijas as $category)
+              <a href="{{ route('productos.categoria', $category->slug) }}">
+                <div class="col-md-4">
 
-          @foreach ($hijas as $category)
-            <a href="{{ route('productos.categoria', $category->slug) }}">
-              <div class="col-md-4">
+                  @if (Storage::disk('public')->exists('categorias/' . $category->imagen))
+                    <img class="w-full h-64 object-cover transform hover:scale-105 transition duration-500 ease-in-out"
+                      src="{{ asset('storage/categorias/' . $category->imagen) }}" alt="{{ $category->categoria }}">
+                  @else
+                    <img class="w-full h-64 object-cover transform hover:scale-105 transition duration-500 ease-in-out"
+                      src="{{ asset('storage/categorias/no_disponible.jpg') }}" alt="Imagen no disponible">
+                  @endif
 
-                @if (Storage::disk('public')->exists('categorias/' . $category->imagen))
-                  <img class="w-full h-64 object-cover transform hover:scale-105 transition duration-500 ease-in-out"
-                    src="{{ asset('storage/categorias/' . $category->imagen) }}" alt="{{ $category->categoria }}">
-                @else
-                  <img class="w-full h-64 object-cover transform hover:scale-105 transition duration-500 ease-in-out"
-                    src="{{ asset('storage/categorias/no_disponible.jpg') }}" alt="Imagen no disponible">
-                @endif
-
-                <div class="">
-                  <h2 class="text-lg font-bold">{{ $category->categoria }}</h2>
+                  <div class="">
+                    <h2 class="text-lg font-bold">{{ $category->categoria }}</h2>
+                  </div>
                 </div>
-              </div>
-            </a>
-          @endforeach
-
-        @endif
-
-
-
-        @if (count($productos) > 0)
-
-          <!-- Mostrar los productos aquí -->
-          @foreach ($productos as $producto)
-            <?php $primeraCategoria = $producto->categorias->first(); ?>
-            <div class="col-md-4">
-              <a href="{{ route('productos.show', [$primeraCategoria->slug, $producto]) }}" class="group">
-                <x-producto :producto="$producto" :fechahoy="$fechahoy" />
               </a>
+            @endforeach
+
+          @endif
+        </div>
+
+        <div class="row">
+          @if (count($productos) > 0)
+
+            <!-- Mostrar los productos aquí -->
+            @foreach ($productos as $producto)
+              <?php $primeraCategoria = $producto->categorias->first(); ?>
+              <div class="col-md-4">
+                <a href="{{ route('productos.show', [$primeraCategoria->slug, $producto]) }}" class="group">
+                  <x-producto :producto="$producto" :fechahoy="$fechahoy" />
+                </a>
+              </div>
+            @endforeach
+
+          @endif
+
+          <!-- Si no tiene productos y tampoco categorias hijas, Muestro el mensaje de alerta -->
+          @if (count($hijas) == 0 and count($productos) == 0)
+            <div class="">
+              <span class="block sm:inline">La categoría no posee aún productos.</span>
             </div>
-          @endforeach
-
-        @endif
-
-        <!-- Si no tiene productos y tampoco categorias hijas, Muestro el mensaje de alerta -->
-        @if (count($hijas) == 0 and count($productos) == 0)
-          <div class="">
-            <span class="block sm:inline">La categoría no posee aún productos.</span>
-          </div>
-        @endif
-
+          @endif
+        </div>
 
       </div>
 
